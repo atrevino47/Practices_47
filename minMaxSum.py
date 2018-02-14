5522ae61f4339262ef5dad4291042fc544e7439a
atrevino47 <36461569+atrevino47@users.noreply.github.com>
2018-02-14 11:26:51 -0600

    Update minMaxSum.py

--- minMaxSum.py
+++ minMaxSum.py
@@ -1,24 +1,20 @@
-...
-Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+#Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
 
-Input Format
+#Input Format
+#A single line of five space-separated integers.
 
-A single line of five space-separated integers.
+#Constraints
+#Each integer is in the inclusive range .
 
-Constraints
+#Output Format
+#Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. (The output can be greater than 32 bit integer.)
 
-Each integer is in the inclusive range .
-Output Format
+#Sample Input
+#1 2 3 4 5
 
-Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. (The output can be greater than 32 bit integer.)
+#Sample Output
+#10 14
 
-Sample Input
-
-1 2 3 4 5
-Sample Output
-
-10 14
-...
 
 import sys
 
